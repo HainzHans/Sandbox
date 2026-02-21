f31e735571fe8f643665dfe82334604d8135c2bd
Johannes <hainz.johannes00@gmail.com>
2026-02-21 15:19:49 +0100

    primeng color fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,20 +9,21 @@ import {definePreset} from '@primeuix/themes';
 const MyPresent = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '#2ecc71',
-      100: '#2ecc71',
-      200: '#2ecc71',
-      300: '#2ecc71',
+      50:  '#e8f9f0',
+      100: '#d1f3e1',
+      200: '#a3e7c3',
+      300: '#75dca5',
       400: '#2ecc71',
       500: '#2ecc71',
       600: '#2ecc71',
-      700: '#2ecc71',
-      800: '#2ecc71',
-      900: '#2ecc71',
-      950: '#2ecc71'
+      700: '#1f9a52',
+      800: '#177b41',
+      900: '#0f5d30',
+      950: '#093e20'
     }
   }
-})
+});
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
